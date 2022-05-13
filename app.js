ea4b7710446de9d45cbadc3d1aa548b074e6cf1c
ema <lotofacundo6@gmail.com>
2022-05-13 11:22:07 -0300

    update code

--- app.js
+++ app.js
@@ -18,6 +18,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
+
+app.get("/", (req, res) => {
+  res.render("index", { titulo: "inicio" });
+});
 
 app.use('/auth', usersRouter);
 app.use('/balance', balanceRouter);
